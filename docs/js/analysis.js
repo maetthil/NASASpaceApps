4407ad6776894feb5e2ada1b17528e6dd62dae53
cazemoises <moises.caze@sou.inteli.edu.br>
2022-10-02 04:11:09 -0300

    att front

--- docs/js/analysis.js
+++ docs/js/analysis.js
@@ -4,20 +4,48 @@ const gradient = ctx.createLinearGradient(0, 0, 0, 400)
 gradient.addColorStop(0, '#5cff')
 gradient.addColorStop(1, '#66ff')
 
-dados = []
-for (n = 1; n < 10; n++) {
-    dados.push(n*10)
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
 }
-console.log(dados)
-const labels = ['teste', 'af', '15', 'f', 'kasdijas']
+
+labels = []
+for (n = 1; n < 50; n++) {
+    labels.push(n)
+}
+dados = []
+for (n = 1; n < 100; n++) {
+    dados.push(getRandomArbitrary(0, 35))
+}
+dados2 = []
+for (n = 1; n < 100; n++) {
+    dados2.push(getRandomArbitrary(-1, 15))
+}
+dados3 = []
+for (n = 100; n > 1; n--) {
+    dados3.push(getRandomArbitrary(25, 45))
+}
+console.log(dados3)
 
 const data = {
     labels,
     datasets: [{
         data: dados,
-        label: "Teste",
-        fill: false
-    }]
+        label: "Bx_GSE",
+        fill: false,
+        borderColor: 'red'
+    }, 
+    {
+        data: dados2,
+        label: "By_GSE",
+        fill: false,
+        borderColor: 'green'
+    },
+    {
+        data: dados3,
+        label: "Bz_GSE",
+        fill: false,
+        borderColor: 'blue'
+    },]
 }
 
 const config = {
